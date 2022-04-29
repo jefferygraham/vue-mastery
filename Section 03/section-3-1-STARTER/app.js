4a1fa29beca35cccc5164cfa936916827e91f189
Jeff Graham <jgraham@vendor.bloombergindustry.com>
2022-04-29 11:03:29 -0400

    Project 1

---
+++ Section 03/section-3-1-STARTER/app.js
@@ -0,0 +1,40 @@
+Vue.createApp({
+  data() {
+    return {
+      perspective: 100,
+      rotateX: 0,
+      rotateY: 0,
+      rotateZ: 0,
+    };
+  },
+  computed: {
+    box() {
+      return {
+        transform: `perspective(${this.perspective}px)
+        rotateX(${this.rotateX}deg)
+        rotateY(${this.rotateY}deg)
+        rotateZ(${this.rotateZ}deg)
+        `,
+      };
+    },
+  },
+  methods: {
+    reset() {
+      this.perspective = 100;
+      this.rotateX = 0;
+      this.rotateY = 0;
+      this.rotateZ = 0;
+    },
+    copy() {
+      const element = document.createElement("textarea");
+      element.setAttribute("readonly", "");
+      element.style.position = "absolute";
+      element.style.left = "-9999px";
+      element.value = `transform: ${this.box.transform}`;
+      document.body.appendChild(element);
+      element.select();
+      document.execCommand("copy");
+      document.body.removeChild(element);
+    },
+  },
+}).mount("#app");
