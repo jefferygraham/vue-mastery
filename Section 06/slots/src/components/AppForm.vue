0cc5387fa38eea016531a30662bcc0bad38e6894
Jeff Graham <jeffery.lemar.graham@gmail.com>
2022-04-30 16:39:04 -0400

    slots

---
+++ Section 06/slots/src/components/AppForm.vue
@@ -0,0 +1,14 @@
+<template>
+  <form action="">
+    <section class="help">
+      <slot name="help"></slot>
+    </section>
+    <section class="fields">
+      <slot name="fields"></slot>
+    </section>
+    <section class="buttons">
+      <slot name="buttons"></slot>
+    </section>
+    <slot></slot>
+  </form>
+</template>
