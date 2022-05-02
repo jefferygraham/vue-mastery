0d5b3c46c71b5ccf1ef9c4b3fd79ae6989197b4c
Jeff Graham <jeffery.lemar.graham@gmail.com>
2022-05-01 20:10:52 -0400

    quiz game

---
+++ Section 08/quiz/src/components/ResultItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="result">
+    <div class="title">{{ results[resultIdx].title }}</div>
+    <div class="desc">{{ results[resultIdx].desc }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ResultItem',
+  props: ['results', 'totalCorrect'],
+  computed: {
+    resultIdx() {
+      let idx = 0;
+      this.results.forEach((result, i) => {
+        if (
+          result.min <= this.totalCorrect &&
+          result.max >= this.totalCorrect
+        ) {
+          idx = i;
+        }
+      });
+      return idx;
+    },
+  },
+};
+</script>
+
+<style></style>
