d8f691ebbbd5f16ea033ccb807d73936e691588f
Jeff Graham <jgraham@vendor.bloombergindustry.com>
2022-05-03 14:16:58 -0400

    form validation

---
+++ music/src/components/LoginForm.vue
@@ -0,0 +1,71 @@
+<template>
+    <div>
+        <div class="text-white text-center font-bold p-5 mb-4" v-if="loggingIn_alert" :class="loggingIn_alert_variant">{{loggingIn_alert_message}}</div>
+          <vee-form :validation-schema="loginSchema" @submit="login">
+            <!-- Email -->
+            <div class="mb-3">
+              <label class="inline-block mb-2">Email</label>
+              <vee-field
+                name="email"
+                type="email"
+                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+                placeholder="Enter Email"
+              />
+              <ErrorMessage class="text-red-600" name="email" />
+            </div>
+            <!-- Password -->
+            <div class="mb-3">
+              <label class="inline-block mb-2">Password</label>
+              <vee-field
+                type="password"
+                name="password"
+                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+                placeholder="Password"
+              />
+              <ErrorMessage class="text-red-600" name="password" />
+            </div>
+            <button
+              type="submit"
+              class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700"
+              :disabled="loggingIn"
+            >
+              Submit
+            </button>
+          </vee-form>
+    </div>
+</template>
+<script>
+export default {
+  name: 'LoginForm',
+  data() {
+    return {
+      loginSchema: {
+        email: 'required|email',
+        password: 'required|min:3|max:32',
+      },
+      userData: {
+        country: "USA",
+      },
+      loggingIn: false,
+      loggingIn_alert: false,
+      loggingIn_alert_variant: 'bg-blue-500',
+      loggingIn_alert_message: 'Please wait. You are being logged in.',
+    };
+  },
+  methods: {
+    login(values) {
+      this.loggingIn = true;
+      this.loggingIn_alert = true;
+      this.loggingIn_alert_variant = "bg-blue-500";
+      this.loggingIn_alert_message = 'Please wait. You are being logged in';
+
+      this.loggingIn_alert_variant = 'bg-green-500';
+      this.loggingIn_alert_message = 'Success! You are now logged in.';
+      console.log(values);
+    },
+  },
+};
+</script>
+<style>
+
+</style>
---
+++ music/src/components/RegistrationForm.vue
@@ -0,0 +1,138 @@
+<template>
+    <div>
+        <!-- Registration Form -->
+          <div class="text-white text-center font-bold p-5 mb-4" v-if="registering_alert" :class="registering_alert_variant">{{registering_alert_message}}</div>
+          <vee-form
+            :validation-schema="schema"
+            @submit="register"
+            :initial-values="userData"
+          >
+            <!-- Name -->
+            <div class="mb-3">
+              <label class="inline-block mb-2">Name</label>
+              <vee-field
+                type="text"
+                name="name"
+                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+                placeholder="Enter Name"
+              />
+              <ErrorMessage class="text-red-600" name="name" />
+            </div>
+            <!-- Email -->
+            <div class="mb-3">
+              <label class="inline-block mb-2">Email</label>
+              <vee-field
+                type="email"
+                name="email"
+                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+                placeholder="Enter Email"
+              />
+              <ErrorMessage class="text-red-600" name="email" />
+            </div>
+            <!-- Age -->
+            <div class="mb-3">
+              <label class="inline-block mb-2">Age</label>
+              <vee-field
+                type="number"
+                name="age"
+                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+              />
+              <ErrorMessage class="text-red-600" name="age" />
+            </div>
+            <!-- Password -->
+            <div class="mb-3">
+              <label class="inline-block mb-2">Password</label>
+              <vee-field
+                name="password" :bails="false" v-slot="{field, errors}">
+                <input type="password" placeholder="password" v-bind="field"
+                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"/>
+                <div class="text-red-600" v-for="error in errors" :key="error">{{error}}</div>
+              </vee-field>
+            </div>
+            <!-- Confirm Password -->
+            <div class="mb-3">
+              <label class="inline-block mb-2">Confirm Password</label>
+              <vee-field
+                type="password"
+                name="confirm_password"
+                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+                placeholder="Confirm Password"
+              />
+              <ErrorMessage class="text-red-600" name="confirm_password" />
+            </div>
+            <!-- Country -->
+            <div class="mb-3">
+              <label class="inline-block mb-2">Country</label>
+              <vee-field
+                as="select"
+                name="country"
+                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+              >
+                <option value="USA">USA</option>
+                <option value="Mexico">Mexico</option>
+                <option value="Germany">Germany</option>
+                <option value="Antarctica">Antarctica</option>
+              </vee-field>
+              <ErrorMessage name="country" class="text-red-600" />
+            </div>
+            <!-- TOS -->
+            <div class="mb-3 pl-6">
+              <vee-field
+                name="tos"
+                value="1"
+                type="checkbox"
+                class="w-4 h-4 float-left -ml-6 mt-1 rounded"
+              />
+              <label class="inline-block">Accept terms of service</label>
+              <ErrorMessage class="text-red-600 block" name="tos" />
+            </div>
+            <button
+              type="submit"
+              :disabled="registering"
+              class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700"
+            >
+              Submit
+            </button>
+          </vee-form>
+    </div>
+</template>
+<script>
+export default {
+  name: 'RegistrationForm',
+  data() {
+    return {
+      schema: {
+        name: 'required|min:3|max:100|alpha_spaces',
+        email: 'required|min:3|max:100|email',
+        age: 'required|min_value:18|max_value:100',
+        password: 'required|min:3|max:32|alpha_num',
+        confirm_password: 'passwords_mismatch:@password',
+        country: 'required|country_excluded:Antarctica',
+        tos: 'tos',
+      },
+      userData: {
+        country: "USA",
+      },
+      registering: false,
+      registering_alert: false,
+      registering_alert_variant: 'bg-blue-500',
+      registering_alert_message: 'Please wait. You account is being prepared.',
+    };
+  },
+  methods: {
+    register(values) {
+      this.registering = true;
+      this.registering_alert = true;
+      this.registering_alert_variant = "bg-blue-500";
+      this.registering_alert_message = 'Please wait. You account is being prepared.';
+
+      this.registering_alert_variant = 'bg-green-500';
+      this.registering_alert_message = 'Success! Your account has been created.';
+      console.log(values);
+    },
+  },
+};
+</script>
+<style>
+
+</style>
