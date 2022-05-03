c69882afb0003e25c1f4986cdabc271fab3a1b16
Jeff Graham <jgraham@vendor.bloombergindustry.com>
2022-05-03 14:51:01 -0400

    install/setup firebase

---
+++ music/src/includes/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import firebase from "firebase/app";
+import "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC0wrMcfHBI8NJXR1OC2WyAqIQQCMzs830",
+  authDomain: "music-20fd6.firebaseapp.com",
+  projectId: "music-20fd6",
+  storageBucket: "music-20fd6.appspot.com",
+  appId: "1:224788380890:web:aeda41029da11e1914b9e3",
+};
+
+// Initialize Firebase
+export default firebase.initializeApp(firebaseConfig);
--- music/src/main.js
+++ music/src/main.js
@@ -1,10 +1,12 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import VeeValidatePlugin from './includes/validation';
-import './assets/tailwind.css';
-import './assets/main.css';
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import VeeValidatePlugin from "./includes/validation";
+
+import "./includes/firebase";
+import "./assets/tailwind.css";
+import "./assets/main.css";
 
 const app = createApp(App);
 
@@ -12,4 +14,4 @@ app.use(store);
 app.use(router);
 app.use(VeeValidatePlugin);
 
-app.mount('#app');
+app.mount("#app");
