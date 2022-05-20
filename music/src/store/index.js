9e8112e946e4b9de87740c75505d44beba3ddd99
Jeff Graham <jg44387@vendor.bna.com>
2022-05-20 10:02:29 -0400

    format time

--- music/src/store/index.js
+++ music/src/store/index.js
@@ -1,5 +1,6 @@
 import { createStore } from 'vuex';
 import { auth, usersCollection } from '@/includes/firebase'; // @ = src
+import helper from '@/includes/helper';
 import { Howl } from 'howler';
 
 export default createStore({
@@ -27,8 +28,8 @@ export default createStore({
       });
     },
     updatePosition(state) {
-      state.seek = state.sound.seek();
-      state.duration = state.sound.duration();
+      state.seek = helper.formatTime(state.sound.seek());
+      state.duration = helper.formatTime(state.sound.duration());
     },
   },
   getters: {
