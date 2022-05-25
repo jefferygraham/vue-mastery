f59d5c87981af436ccbe61453694b35cdedaa684
Jeff Graham <jeffery.lemar.graham@gmail.com>
2022-05-25 18:45:04 -0400

    diretive modifiers

--- music/src/directives/icons.js
+++ music/src/directives/icons.js
@@ -1,9 +1,19 @@
 export default {
   beforeMount(element, binding) {
-    let iconClass = `fa fa-${binding.value} float-right text-green-400 text-2xl`;
+    let iconClass = `fa fa-${binding.value} text-2xl`;
 
     if (binding.arg === 'full') {
       iconClass = binding.value;
+    }
+
+    if (binding.modifiers.right) {
+      iconClass += ' float-right';
+    }
+
+    if (binding.modifiers.yellow) {
+      iconClass += ' text-cyan-400';
+    } else {
+      iconClass += ' text-green-400';
     }
 
     // eslint-disable-next-line no-param-reassign
