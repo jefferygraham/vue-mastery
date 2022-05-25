d35cc584eb43d72ce548c4b72959c8a84d10432d
Jeff Graham <jeffery.lemar.graham@gmail.com>
2022-05-25 18:11:00 -0400

    add directive for icons

---
+++ music/src/directives/icons.js
@@ -0,0 +1,7 @@
+export default {
+  beforeMount(element) {
+    // eslint-disable-next-line no-param-reassign
+    element.innerHTML +=
+      '<i class="fa fa-headphones-alt float-right text-green-400 text-2xl"></i>';
+  },
+};
