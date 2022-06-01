2379f2baadca2a0059f595d2f3aebde8d57174bb
Jeff Graham <jg44387@vendor.bna.com>
2022-06-01 11:03:23 -0400

    first translation

--- music/src/views/SongView.vue
+++ music/src/views/SongView.vue
@@ -65,7 +65,9 @@
             </button>
           </vee-form>
           <!-- Sort Comments -->
+          <label for="sort"></label>
           <select
+            name="sort"
             class="block mt-4 py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
             v-model="sort"
           >
